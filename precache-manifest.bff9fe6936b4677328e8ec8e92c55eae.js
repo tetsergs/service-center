a48c65b44b9a7e57f8709917343be28274a6da9c
tetsergs <tetsergs123@gmail.com>
2025-05-16 18:03:14 +0500

    Updates

---
+++ precache-manifest.bff9fe6936b4677328e8ec8e92c55eae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3351083b431391a76c0a77fab81075d6",
+    "url": "/service-center/index.html"
+  },
+  {
+    "revision": "3113d568fc9a53142a29",
+    "url": "/service-center/static/css/3.de1632da.chunk.css"
+  },
+  {
+    "revision": "8c97bf3cb389309e4415",
+    "url": "/service-center/static/js/2.3c714cb9.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/service-center/static/js/2.3c714cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3113d568fc9a53142a29",
+    "url": "/service-center/static/js/3.e5be2207.chunk.js"
+  },
+  {
+    "revision": "59c380a03a1a4b17b2f270c8c67f4be7",
+    "url": "/service-center/static/js/3.e5be2207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb74c28bbe1906f32be",
+    "url": "/service-center/static/js/4.7bd871eb.chunk.js"
+  },
+  {
+    "revision": "036dc6738e2543acd8cf9b1a224d0765",
+    "url": "/service-center/static/js/4.7bd871eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9877c1d9d3fd0fdc6c91",
+    "url": "/service-center/static/js/5.f7d36252.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/service-center/static/js/5.f7d36252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7907a5e65e59376c0d24",
+    "url": "/service-center/static/js/main.aedbfbd0.chunk.js"
+  },
+  {
+    "revision": "1b2b43dadf005a60cd47",
+    "url": "/service-center/static/js/runtime-main.a4cde660.js"
+  }
+]);

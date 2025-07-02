86a70463aff06c907d435dff6f1d149fb293b919
tetsergs <tetsergs123@gmail.com>
2025-07-02 15:36:59 +0500

    pizdec

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ReportsPage from './pages/ReportsPage';
 import WarrantyPage from './pages/WarrantyPage';
 import DefectRepairPage from './pages/DefectRepairPage';
 import EditPrices from './pages/EditPrices';
+import WriteOffPage from './pages/WriteOffPage'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <NavLink className="btn me-2" to="/warranty" style={({ isActive }) => ({ backgroundColor: isActive ? '#0d6efd' : '#6c757d', color: '#fff' })}>Гарантия</NavLink>
           <NavLink className="btn me-2" to="/defect" style={({ isActive }) => ({ backgroundColor: isActive ? '#0d6efd' : '#6c757d', color: '#fff' })}>Брак Склад</NavLink>
           <NavLink className="btn me-2" to="/edit" style={({ isActive }) => ({ backgroundColor: isActive ? '#0d6efd' : '#6c757d', color: '#fff' })}>Цены</NavLink>
+          <NavLink className="btn me-2" to="/writeoff" style={({ isActive }) => ({ backgroundColor: isActive ? '#0d6efd' : '#6c757d', color: '#fff' })}>СЗ по Браку</NavLink>
         </nav>
         <Routes>
           <Route path="/" element={<ReceptionPage />} />
@@ -26,6 +28,7 @@ function App() {
           <Route path="/warranty" element={<WarrantyPage />} />
           <Route path="/defect" element={<DefectRepairPage />} />
           <Route path="/edit" element={<EditPrices />} />
+          <Route path="/writeoff" element={<WriteOffPage />} />
         </Routes>
       </div>
     </Router>
